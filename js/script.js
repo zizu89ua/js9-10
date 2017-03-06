bb3753aeaa976bf7b3f4520d0ca68dbe869653b6
Dmytro Kharchenko <dimonkharchenko89@gmail.com>
2017-03-06 18:21:01 +0200

    added files and slider select checkbox blocks

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(function() {
+    $('.jcarousel').jcarousel({
+    });
+    $('select').niceSelect();
+    $(".cb-enable").click(function(){
+        var parent = $(this).parents('.switch');
+        $('.cb-disable',parent).removeClass('selected');
+        $(this).addClass('selected');
+        $('.checkbox',parent).attr('checked', true);
+    });
+    $(".cb-disable").click(function(){
+        var parent = $(this).parents('.switch');
+        $('.cb-enable',parent).removeClass('selected');
+        $(this).addClass('selected');
+        $('.checkbox',parent).attr('checked', false);
+    });
+    $('.menu__li').hover(function(){
+    	$(this).children('.menu__sub').animate({
+    		opacity: 'show'
+    	}, 1000);
+    })
+});
